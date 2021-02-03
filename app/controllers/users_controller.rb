b6e6dd3af0dd495ac4e8c41f8dc31c6055946e18
Amanda <amandaben21@aol.com>
2021-02-02 21:40:25 -0500

    put helper methods in app controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,13 +52,5 @@ class UsersController < ApplicationController
     redirect "/users"
   end
 
-  helpers do
-    def logged_in?
-      !!current_user
-    end
 
-    def current_user
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
-  end
 end
