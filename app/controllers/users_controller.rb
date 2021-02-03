b4a9dfdd7144d84529ce2c8993998e6448711357
Amanda <amandaben21@aol.com>
2021-02-03 03:36:30 -0500

    Fixed errors and change color background

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,23 +28,4 @@ class UsersController < ApplicationController
     
   end
 
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
-  end
-
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
-
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
-
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
-  end
 end
