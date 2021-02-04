7636f567d9196f44bc8af311db25145a4ebea04c
Amanda <amandaben21@aol.com>
2021-02-03 23:48:33 -0500

    fixed mispelling

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -16,49 +16,64 @@ class NotesController < ApplicationController
   # POST: /notes
   post "/notes" do
     redirect_if_not_logged_in
-    redirect "/notes"
+    @note = Note.create(params[:note])
+    if @note.valid?
+      redirect "/notes/#{@note.id}"
+    else
+      redirect "/notes/new"
+    end
   end
 
   # GET: /notes/5
   get "/notes/:id" do
     redirect_if_not_logged_in
-    erb :"/notes/show.html"
+    @note = Note.find_by_id(params[:id])
+    if @note
+     erb :"/notes/show.html"
+    else
+      redirect "/notes"
+    end
   end
 
   # GET: /notes/5/edit
   get "/notes/:id/edit" do
     redirect_if_not_logged_in
-    @note = current_user.note.find_by_id(params[:id])
+    find_current_users_note
     if @note
       erb :"/notes/edit.html"
     else
-      redirect "/notes/#{@note.id}"
+      redirect "/notes"
     end
   end
 
   # PATCH: /notes/5
   patch "/notes/:id" do
     redirect_if_not_logged_in
-    redirect "/notes/:id"
+    find_current_users_note
+    if @note
+      @note.update(params[:note])
+      redirect "/notes/#{@note.id}"
+    else
+      redirect "/"
+    end
   end
 
   # DELETE: /notes/5/delete
   delete "/notes/:id/delete" do
     redirect_if_not_logged_in
-    
-    @note = current_user.notes.find_by_id(params[:id])
+    find_current_users_note
     if @note
       @note.destroy
       redirect "/notes"
     else
-      redirect "/notes/ #{@note.id}"
+      redirect "/notes/#{@note.id}"
     end
   end
 
 
   private
-  def find_note
-    @note = Note.find_by_id(params[:id])
+  def find_current_users_note
+    @note = current_user.notes.find_by_id(params[:id])
   end
 end
 
